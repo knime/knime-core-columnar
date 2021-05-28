14eb79fb32bece4fa638ac778feaadd966adc9bf
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2021-05-28 13:41:53 +0200

    AP-16650: Set LZ4 compression as default
    AP-16650 (Re-enable compression)
    
    
    (This commit was moved from knime-core-arrow. Original id: d0bdf9c80c8419cbd5ff9c355fe3fbf7afbd103f)

--- org.knime.core.columnar.arrow/src/main/java/org/knime/core/columnar/arrow/compress/ArrowCompressionUtil.java
+++ org.knime.core.columnar.arrow/src/main/java/org/knime/core/columnar/arrow/compress/ArrowCompressionUtil.java
@@ -94,15 +94,21 @@ public final class ArrowCompressionUtil {
     public static ArrowCompression getDefaultCompression() {
         if (defaultCompression == null) {
             final String compressionName = System.getProperty(PROPERTY_COMPRESSION);
-            if (compressionName == null || PROPERTY_COMPRESSION_NONE.equals(compressionName)) {
+            if (PROPERTY_COMPRESSION_NONE.equals(compressionName)) {
+                // Property set to LZ4
                 defaultCompression = ARROW_NO_COMPRESSION;
             } else if (PROPERTY_COMPRESSION_LZ4.equals(compressionName)) {
+                // Property set to NONE
                 defaultCompression = ARROW_LZ4_BLOCK_COMPRESSION;
             } else {
-                LOGGER.error(
-                    "Invalid Arrow compression format '{}'. Valid options are ({}|{}). Using the default '{}'.",
-                    compressionName, PROPERTY_COMPRESSION_LZ4, PROPERTY_COMPRESSION_NONE, PROPERTY_COMPRESSION_NONE);
-                defaultCompression = ARROW_NO_COMPRESSION;
+                if (compressionName != null) {
+                    // Property set but nothing matched
+                    LOGGER.error(
+                        "Invalid Arrow compression format '{}'. Valid options are ({}|{}). Using the default '{}'.",
+                        compressionName, PROPERTY_COMPRESSION_LZ4, PROPERTY_COMPRESSION_NONE, PROPERTY_COMPRESSION_LZ4);
+                }
+                // Using the default
+                defaultCompression = ARROW_LZ4_BLOCK_COMPRESSION;
             }
         }
         return defaultCompression;
