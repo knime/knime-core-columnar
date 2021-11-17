1f12cd43ad35cabf78bac953beb0e6dca4c968e8
Adrian Nembach <adrian.nembach@knime.com>
2021-11-17 09:49:39 +0100

    AP-17723: Track open RowAccessibles in AbstractColumnarContainerTable
    This allows us to close the RowAccessibles when this table is closed.
    Downstream clients might run into problems if that happens but it's
    better than the memory leak that is the alternative.
    
    AP-17723 (Memory Leaks when temporary tables are cleared out-of-order)

--- org.knime.core.data.columnar/src/main/java/org/knime/core/data/columnar/table/AbstractColumnarContainerTable.java
+++ org.knime.core.data.columnar/src/main/java/org/knime/core/data/columnar/table/AbstractColumnarContainerTable.java
@@ -117,6 +117,8 @@ abstract class AbstractColumnarContainerTable extends ExtensionTable implements 
 
     private final Set<Finalizer> m_openCursorFinalizers = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
+    private final CloseableTracker<RowAccessible, IOException> m_trackedRowAccessibles = new CloseableTracker<>(IOException.class);
+
     // effectively final
     private Finalizer m_tableCloser;
 
@@ -203,6 +205,7 @@ abstract class AbstractColumnarContainerTable extends ExtensionTable implements 
         }
         m_openCursorFinalizers.clear();
         try {
+            m_trackedRowAccessibles.close();
             m_columnarTable.close();
         } catch (final IOException e) {
             LOGGER.error(String.format("Exception while clearing ContainerTable: %s", e.getMessage()), e);
@@ -250,8 +253,9 @@ abstract class AbstractColumnarContainerTable extends ExtensionTable implements 
         return iterator;
     }
 
+    @SuppressWarnings("resource") // we are tracking it so that we can close it when this table is closed
     RowAccessible asRowAccessible() {
-        return new ColumnarContainerRowAccessible();
+        return m_trackedRowAccessibles.track(new ColumnarContainerRowAccessible());
     }
 
     private final class ColumnarContainerRowAccessible implements RowAccessible {
