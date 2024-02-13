c482c0177edab50465b718805654400240de2a36
tpietzsch <tobias.pietzsch@gmail.com>
2024-02-13 20:14:42 +0100

    try forwarding() inside finish()

--- org.knime.core.columnar/src/main/java/org/knime/core/columnar/badger/HeapBadger.java
+++ org.knime.core.columnar/src/main/java/org/knime/core/columnar/badger/HeapBadger.java
@@ -762,6 +762,7 @@ public class HeapBadger {
             }
             try {
                 debug("[c:" + this + "]   -> m_queue.finish()");
+                forward();
                 m_queue.finish();
                 m_closed = true;
                 debug("[c:" + this + "]   <- m_queue.finish()");
