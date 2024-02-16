f13b387bc6e53b6e6e14f054c7c9cfd0ece1f86e
tpietzsch <tobias.pietzsch@gmail.com>
2024-02-16 18:09:31 +0100

    Use copy of WriteAccessRowWrite in RowIteratorCursor (temporary)

--- org.knime.core.data.columnar/src/main/java/org/knime/core/data/columnar/table/virtual/RowIteratorCursor.java
+++ org.knime.core.data.columnar/src/main/java/org/knime/core/data/columnar/table/virtual/RowIteratorCursor.java
@@ -49,9 +49,15 @@
 package org.knime.core.data.columnar.table.virtual;
 
 import java.io.IOException;
+import java.util.stream.IntStream;
 
+import org.knime.core.data.RowKeyValue;
 import org.knime.core.data.container.CloseableRowIterator;
 import org.knime.core.data.container.DataRowRead;
+import org.knime.core.data.v2.RowKeyWriteValue;
+import org.knime.core.data.v2.RowRead;
+import org.knime.core.data.v2.RowWrite;
+import org.knime.core.data.v2.WriteValue;
 import org.knime.core.data.v2.schema.ValueSchema;
 import org.knime.core.table.access.BufferedAccesses;
 import org.knime.core.table.access.BufferedAccesses.BufferedAccessRow;
@@ -59,6 +65,7 @@ import org.knime.core.table.cursor.Cursor;
 import org.knime.core.table.cursor.LookaheadCursor;
 import org.knime.core.table.row.ReadAccessRow;
 import org.knime.core.table.row.Selection.ColumnSelection;
+import org.knime.core.table.row.WriteAccessRow;
 
 /**
  * A {@link Cursor} that is backed by a {@link CloseableRowIterator}.
@@ -81,6 +88,94 @@ final class RowIteratorCursor implements LookaheadCursor<ReadAccessRow> {
         m_rowRead = new DataRowRead();
         m_rowWrite = new WriteAccessRowWrite(schema, m_accessRow, selection);
         m_iterator = iterator;
+    }
+
+
+    private static class WriteAccessRowWrite implements RowWrite {
+
+        // TODO with a bit of refactoring this could also be used in BufferedRowContainer
+
+        private final WriteAccessRow m_accesses;
+
+        private final WriteValue<?>[] m_values;
+
+        private final RowKeyWriteValue m_rowKeyValue;
+
+        /**
+         * Constructor.
+         *
+         * @param schema of the table
+         * @param writeAccess to write to
+         * @param selection the selected columns
+         */
+        public WriteAccessRowWrite(final ColumnarValueSchema schema, final WriteAccessRow writeAccess,
+            final ColumnSelection selection) {
+            m_accesses = writeAccess;
+            final var numColumns = schema.numColumns();
+            m_values = new WriteValue<?>[numColumns];
+            var selected = selection.allSelected() ? IntStream.range(0, numColumns)
+                : IntStream.of(selection.getSelected(0, numColumns));
+            selected.forEach(i -> m_values[i] = schema.getValueFactory(i).createWriteValue(m_accesses.getWriteAccess(i)));
+            m_rowKeyValue = (RowKeyWriteValue)m_values[0];
+        }
+
+        /**
+         * Constructor.
+         *
+         * @param schema of the table
+         * @param writeAccess to write to
+         */
+        public WriteAccessRowWrite(final ColumnarValueSchema schema, final WriteAccessRow writeAccess) {
+            this(schema, writeAccess, ColumnSelection.all());
+        }
+
+        @Override
+        public void setFrom(final RowRead values) {
+            assert values.getNumColumns() == getNumColumns();
+            setRowKey(values.getRowKey());
+            for (var i = 0; i < values.getNumColumns(); i++) {
+                if (values.isMissing(i)) {
+                    setMissing(i);
+                } else {
+                    final var writeValue = m_values[i + 1];
+                    if (writeValue != null) {
+                        writeValue.setValue(values.getValue(i));
+                    }
+                }
+            }
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public <W extends WriteValue<?>> W getWriteValue(final int index) {
+            return (W)m_values[index + 1];
+        }
+
+        @Override
+        public int getNumColumns() {
+            return m_values.length - 1;
+        }
+
+        @Override
+        public void setMissing(final int index) {
+            if (m_values[index + 1] != null) {
+                m_accesses.getWriteAccess(index + 1).setMissing();
+            }
+        }
+
+        @Override
+        public void setRowKey(final String rowKey) {
+            if (m_rowKeyValue != null) {
+                m_rowKeyValue.setRowKey(rowKey);
+            }
+        }
+
+        @Override
+        public void setRowKey(final RowKeyValue rowKey) {
+            if (m_rowKeyValue != null) {
+                m_rowKeyValue.setRowKey(rowKey);
+            }
+        }
     }
 
     RowIteratorCursor(final ValueSchema schema, final CloseableRowIterator iterator) {
