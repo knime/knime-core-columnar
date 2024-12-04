345bc57eef27d3783eb5fd4538259b6b1df80897
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-12-04 14:34:26 +0100

    WIP

--- org.knime.core.columnar.arrow/src/main/java/org/knime/core/columnar/arrow/data/OffsetsBuffer.java
+++ org.knime.core.columnar.arrow/src/main/java/org/knime/core/columnar/arrow/data/OffsetsBuffer.java
@@ -1,6 +1,7 @@
 /*
  * ------------------------------------------------------------------------
  *
+
  *  Copyright by KNIME AG, Zurich, Switzerland
  *  Website: http://www.knime.com; Email: contact@knime.com
  *
@@ -48,86 +49,160 @@
  */
 package org.knime.core.columnar.arrow.data;
 
+import java.util.Arrays;
+
 import org.apache.arrow.memory.ArrowBuf;
 
 /**
+ * Holds the offsets of elements in a buffer or array.
+ * <P>
+ * For example, the offsets {@code [0, 3, 7, 10, 13]} indicate:
+ * <ul>
+ * <li>Element 0 is at indices 0 to 3 (exclusive of 3)</li>
+ * <li>Element 1 is at indices 3 to 7 (exclusive of 7)</li>
+ * <li>Element 2 is at indices 7 to 10 (exclusive of 10)</li>
+ * <li>Element 3 is at indices 10 to 13 (exclusive of 13)</li>
+ * </ul>
+ * <P>
+ * This is in line with the Arrow specification and can be copied to and from an Arrow buffer.
+ *
  * @author Benjamin Wilhelm, KNIME GmbH, Berlin, Germany
  */
-public final class OffsetsBuffer {
+final class OffsetsBuffer {
 
     private int[] m_offsets;
 
     private int m_nextIndex;
 
+    /**
+     * Computes the size in bytes required to store the offsets for a given capacity.
+     *
+     * @param capacity the number of elements
+     * @return the size in bytes required
+     */
     public static int usedSizeFor(final int capacity) {
         return (capacity + 1) * Integer.BYTES;
     }
 
+    /**
+     * Constructs an {@code OffsetsBuffer} with the specified capacity.
+     *
+     * @param capacity the number of elements
+     */
     public OffsetsBuffer(final int capacity) {
         m_offsets = new int[capacity + 1];
         m_nextIndex = 0;
     }
 
-    public OffsetsBuffer(final int[] offsets) {
-        m_offsets = offsets;
-        m_nextIndex = offsets.length - 1;
+    /**
+     * Constructs an {@code OffsetsBuffer} from an existing offsets array.
+     *
+     * @param offsets the offsets array
+     */
+    OffsetsBuffer(final int[] offsets) {
+        this.m_offsets = Arrays.copyOf(offsets, offsets.length);
+        this.m_nextIndex = offsets.length - 1;
     }
 
+    /**
+     * Sets the number of elements and resizes the offsets array accordingly.
+     *
+     * @param numElements the new number of elements
+     */
     public void setNumElements(final int numElements) {
-        var newOffsets = new int[numElements + 1];
-        System.arraycopy(m_offsets, 0, newOffsets, 0, Math.min(m_offsets.length, numElements + 1));
-        m_offsets = newOffsets;
-
-        // Update the next index if it is out of bounds
+        m_offsets = Arrays.copyOf(m_offsets, numElements + 1);
         m_nextIndex = Math.min(m_nextIndex, numElements);
     }
 
+    /**
+     * Returns the last offset value.
+     *
+     * @return the last offset
+     */
     public int getLastOffset() {
         return m_offsets[m_nextIndex];
     }
 
-    public int putValue(final int index, final int size) {
-        var startOffset = getLastOffset();
-        var endOffset = startOffset + size;
+    /**
+     * Updates the offsets array at the specified index with the given size.
+     *
+     * @param index the index at which to set the offset
+     * @param size the size of the value
+     * @return the end offset after inserting the value
+     */
+    public int setOffsetAtIndex(final int index, final int size) {
+        int startOffset = getLastOffset();
+        int endOffset = startOffset + size;
 
         // Fill holes with the start offset
-        for (int i = m_nextIndex + 1; i <= index; i++) {
-            m_offsets[i] = startOffset;
-        }
+        Arrays.fill(m_offsets, m_nextIndex + 1, index + 1, startOffset);
+
         m_nextIndex = index + 1;
         m_offsets[m_nextIndex] = endOffset;
 
         return endOffset;
     }
 
-    public void endBuffer(final int length) {
-        var lastOffset = getLastOffset();
+    /**
+     * Completes the buffer by filling any remaining offsets up to the specified length.
+     *
+     * @param length the total number of elements
+     */
+    public void completeBuffer(final int length) {
+        int lastOffset = getLastOffset();
 
-        // Fill hole in the offset buffer if necessary
-        for (int i = m_nextIndex + 1; i <= length; i++) {
-            m_offsets[i] = lastOffset;
-        }
+        // Fill holes in the offset buffer if necessary
+        Arrays.fill(m_offsets, m_nextIndex + 1, length + 1, lastOffset);
         m_nextIndex = length;
     }
 
+    /**
+     * Returns the start index for the specified element index.
+     *
+     * @param index the element index
+     * @return the start offset
+     */
     public int getStartIndex(final int index) {
         return m_offsets[index];
     }
 
+    /**
+     * Returns the end index for the specified element index.
+     *
+     * @param index the element index
+     * @return the end offset
+     */
     public int getEndIndex(final int index) {
         return m_offsets[index + 1];
     }
 
+    /**
+     * Returns the size in bytes of the offsets buffer.
+     *
+     * @return the size in bytes
+     */
     public int sizeOf() {
         return m_offsets.length * Integer.BYTES;
     }
 
+    /**
+     * Copies the offsets to the specified Arrow buffer.
+     *
+     * @param offsetBuffer the target Arrow buffer
+     */
     public void copyTo(final ArrowBuf offsetBuffer) {
         MemoryCopyUtils.copy(m_offsets, offsetBuffer);
     }
 
+    /**
+     * Creates an {@code OffsetsBuffer} from an Arrow buffer.
+     *
+     * @param offsetBuffer the source Arrow buffer
+     * @param length the number of elements
+     * @return a new {@code OffsetsBuffer} initialized from the Arrow buffer
+     */
     public static OffsetsBuffer createFrom(final ArrowBuf offsetBuffer, final int length) {
-        var offsets = new int[length + 1];
+        int[] offsets = new int[length + 1];
         MemoryCopyUtils.copy(offsetBuffer, offsets);
         return new OffsetsBuffer(offsets);
     }
