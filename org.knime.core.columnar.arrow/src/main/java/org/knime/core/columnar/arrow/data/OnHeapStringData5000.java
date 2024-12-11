cf1fef4b6cea7fd7299ffdb4e9ff46b5979f0946
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-12-11 16:00:44 +0100

    WIP - serialize on access string data

---
+++ org.knime.core.columnar.arrow/src/main/java/org/knime/core/columnar/arrow/data/OnHeapStringData5000.java
@@ -0,0 +1,393 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   Sep 20, 2024 (benjamin): created
+ */
+package org.knime.core.columnar.arrow.data;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.function.LongSupplier;
+
+import org.apache.arrow.vector.FieldVector;
+import org.apache.arrow.vector.VarCharVector;
+import org.apache.arrow.vector.dictionary.DictionaryProvider;
+import org.apache.arrow.vector.types.Types.MinorType;
+import org.apache.arrow.vector.types.pojo.Field;
+import org.knime.core.columnar.arrow.ArrowColumnDataFactoryVersion;
+import org.knime.core.columnar.arrow.data.OffsetsBuffer5000.IntOffsetsBuffer;
+import org.knime.core.columnar.data.NullableReadData;
+import org.knime.core.columnar.data.StringData.StringReadData;
+import org.knime.core.columnar.data.StringData.StringWriteData;
+import org.knime.core.table.util.StringEncoder;
+
+/**
+ *
+ * @author Benjamin Wilhelm, KNIME GmbH, Berlin, Germany
+ */
+public final class OnHeapStringData5000 {
+
+    interface CopyableStringWriteData extends StringWriteData {
+        void setStringBytes(int index, byte[] val);
+
+        void setFrom(CopyableStringReadData sourceData, int sourceIndex, int targetIndex);
+    }
+
+    interface CopyableStringReadData extends StringReadData {
+        byte[] getStringBytesNullable(int index);
+    }
+
+    public static final Factory FACTORY = new Factory();
+
+    private OnHeapStringData5000() {
+    }
+
+    /**
+     * Encodes the given strings into the target byte array. Starts at the string
+     *
+     * @param source the strings to encode
+     * @param startIdx the index of the fist string to encode
+     * @param target the target byte array
+     * @param offsets the offsets buffer to keep track of the offsets in the target array
+     * @param length the number of strings to copy
+     */
+    private static void encode(final String[] source, final int startIdx, final byte[] target,
+        final IntOffsetsBuffer offsets, final int length) {
+        var encoder = new StringEncoder();
+        for (var i = startIdx; i < startIdx + length; i++) {
+            var str = source[i];
+            var bytes = encoder.encode(str).array(); // TODO encode directly into the target array?
+            var dataIndex = offsets.add(i, bytes.length);
+            System.arraycopy(bytes, 0, target, dataIndex.start(), bytes.length);
+        }
+    }
+
+    /**
+     * Holds the data of a string column in a combination of a string array and a byte array.
+     *
+     * Note that none of the two representations is guaranteed to be complete to the last set index.
+     *
+     * The byte[] is complete up to a certain index (the last one that was encoded) ({@link #lastWrittenBytesIndex()})
+     * which is tracked by the offsets buffer. The String[] is sparsely filled. However, it is guaranteed to contain all
+     * values after the {@link #lastWrittenBytesIndex()}.
+     */
+    public static final class OnHeapStringWriteData extends AbstractArrowWriteData implements CopyableStringWriteData {
+
+        private String[] m_data;
+
+        private byte[] m_dataBytes;
+
+        private IntOffsetsBuffer m_offsets; // TODO include in size
+
+        /**
+         * @return the index of the last element that was written to the <code>m_dataBytes</code> array. Note that
+         *         <code>m_data</code> can contain values with larger indices.
+         */
+        private int lastWrittenBytesIndex() {
+            return m_offsets.lastWrittenIndex();
+        }
+
+        private OnHeapStringWriteData(final int capacity) {
+            super(capacity);
+            m_data = new String[capacity];
+            m_dataBytes = new byte[0];
+            m_offsets = OffsetsBuffer5000.createIntBuffer(capacity);
+        }
+
+        private OnHeapStringWriteData(final int offset, final String[] data, final ValidityBuffer validity) {
+            super(offset, validity);
+            m_data = data;
+            // TODO pass data bytes and offsets buffer
+        }
+
+        @Override
+        public void expand(final int minimumCapacity) {
+            setNumElements(minimumCapacity);
+        }
+
+        @Override
+        public int capacity() {
+            return m_data.length;
+        }
+
+        @Override
+        public long usedSizeFor(final int numElements) {
+            // TODO das ist doch quatsch, oder?
+            return numElements * Integer.BYTES + ValidityBuffer.usedSizeFor(numElements);
+        }
+
+        @Override
+        public long sizeOf() {
+            // TODO das ist doch quatsch, oder?
+            return m_data.length * Integer.BYTES + m_validity.sizeOf();
+        }
+
+        @Override
+        public ArrowWriteData slice(final int start) {
+            return new OnHeapStringWriteData(m_offset + start, m_data, m_validity);
+        }
+
+        @Override
+        public void setString(final int index, final String val) {
+            m_data[index + m_offset] = val;
+            setValid(index + m_offset);
+        }
+
+        @Override
+        protected void closeResources() {
+            // No resources to close for on-heap data
+        }
+
+        @Override
+        public OnHeapStringReadData close(final int length) {
+            setNumElements(length);
+            // TODO give the offsets buffer and byte array to the read data
+            return new OnHeapStringReadData(m_data, m_dataBytes, m_offsets, m_validity);
+        }
+
+        /**
+         * Expand or shrink the data to the given size.
+         *
+         * @param numElements the new size of the data
+         */
+        private void setNumElements(final int numElements) {
+            m_validity.setNumElements(numElements);
+
+            var newData = new String[numElements];
+            System.arraycopy(m_data, 0, newData, 0, Math.min(m_data.length, numElements));
+            m_data = newData;
+
+            m_offsets.setNumElements(numElements);
+        }
+
+        // LOW LEVEL COPY
+
+        @Override
+        public void setStringBytes(final int index, final byte[] val) {
+            var idx = m_offset + index;
+
+            // Note - we prefer to encode the previous values to decoding this one, because
+            // - it is likely that we need the byte representation of the previous values anyway
+            // - we have two representations of the data in the newer data object, so this will be more flexible and
+            //   efficient to work with
+
+            // Encode the values that not yet encoded
+            var lastBytesIdx = lastWrittenBytesIndex();
+            if (lastBytesIdx < idx - 1) {
+                // Encode the values up to the last index
+                encode(m_data, lastBytesIdx + 1, m_dataBytes, m_offsets, idx - lastBytesIdx - 1);
+            }
+
+            // Copy over the new value
+            var dataIndex = m_offsets.add(idx, val.length);
+            System.arraycopy(val, 0, m_dataBytes, dataIndex.start(), val.length);
+            setValid(idx);
+        }
+
+        @Override
+        public void setFrom(final CopyableStringReadData sourceData, final int sourceIndex, final int targetIndex) {
+            if (sourceData instanceof OnHeapStringReadData onHeapStringData) {
+                if (onHeapStringData.isMissing(sourceIndex)) {
+                    setMissing(targetIndex + m_offset);
+                } else {
+                    // Copy the string
+                    // Note that this can be null if the data was just read and not yet decoded. In this case, the
+                    // bytes will be set and the string will be decoded on access
+                    m_data[targetIndex + m_offset] = onHeapStringData.getString(sourceIndex);
+
+                    // Copy the bytes if they are present (must be present if the string is null)
+                    if (onHeapStringData.m_offsets.lastWrittenIndex() >= sourceIndex) {
+                        var sourceDataIndex = onHeapStringData.m_offsets.get(sourceIndex);
+                        var targetDataIndex = m_offsets.add(targetIndex + m_offset, sourceDataIndex.length());
+                        System.arraycopy(onHeapStringData.m_dataBytes, sourceDataIndex.start(), m_dataBytes,
+                            targetDataIndex.start(), sourceDataIndex.length());
+                    }
+
+                }
+            }
+        }
+    }
+
+    public static final class OnHeapStringReadData extends AbstractArrowReadData implements CopyableStringReadData {
+
+        // TODO is this better than just using String.decode()?
+        private final StringEncoder encoder = new StringEncoder();
+
+        private final String[] m_data;
+
+        private final byte[] m_dataBytes;
+
+        private final IntOffsetsBuffer m_offsets;
+
+        public OnHeapStringReadData(final String[] data, final byte[] dataBytes, final IntOffsetsBuffer offsets,
+            final ValidityBuffer validity) {
+            super(validity, data.length);
+            m_data = data;
+            m_dataBytes = dataBytes;
+            m_offsets = offsets;
+        }
+
+        public OnHeapStringReadData(final String[] data, final ValidityBuffer validity, final int offset,
+            final int length) {
+            super(validity, offset, length);
+            m_data = data;
+            m_dataBytes = null; // TODO pass objects for slice
+            m_offsets = null;
+        }
+
+        @Override
+        public long sizeOf() {
+            // TODO size of the strings
+            return m_data.length * Integer.BYTES + m_validity.sizeOf();
+        }
+
+        @Override
+        public ArrowReadData slice(final int start, final int length) {
+            return new OnHeapStringReadData(m_data, m_validity, m_offset + start, length);
+        }
+
+        @Override
+        public String getString(final int index) {
+            // Assuming not missing (the caller has to check)
+
+            if (m_data[index + m_offset] == null) {
+                // Decode the string
+                var dataIndex = m_offsets.get(index + m_offset);
+
+                // TODO is this correct
+                var val = encoder.decode(ByteBuffer.wrap(m_dataBytes, dataIndex.start(), dataIndex.length()));
+
+                m_data[index + m_offset] = val;
+            }
+
+            return m_data[index + m_offset];
+        }
+
+        @Override
+        public byte[] getStringBytesNullable(final int index) {
+            if (!isMissing(index + m_offset) && m_offsets.lastWrittenIndex() >= index + m_offset) {
+                var dataIndex = m_offsets.get(index + m_offset);
+                var bytes = new byte[dataIndex.length()];
+                System.arraycopy(m_dataBytes, dataIndex.start(), bytes, 0, bytes.length);
+                return bytes;
+            } else {
+                // TODO should we encode the data now?
+                return null;
+            }
+        }
+    }
+
+    public static final class Factory extends AbstractArrowColumnDataFactory {
+
+        private Factory() {
+            super(ArrowColumnDataFactoryVersion.version(0));
+        }
+
+        @Override
+        public OnHeapStringReadData createRead(final FieldVector vector, final ArrowVectorNullCount nullCount,
+            final DictionaryProvider provider, final ArrowColumnDataFactoryVersion version) throws IOException {
+
+            // TODO what is the null count for???
+
+            if (m_version.equals(version)) {
+
+                var valueCount = vector.getValueCount();
+                var v = (VarCharVector)vector;
+                var validity = ValidityBuffer.createFrom(vector.getValidityBuffer(), valueCount);
+                var offsets = OffsetsBuffer5000.createIntBuffer(vector.getOffsetBuffer(), valueCount);
+                var dataBytes = new byte[offsets.getNumData(valueCount)];
+                vector.getDataBuffer().getBytes(0, dataBytes);
+
+                return new OnHeapStringReadData(new String[valueCount], dataBytes, offsets, validity);
+            } else {
+                throw new IOException(
+                    "Cannot read ArrowStringData with version " + version + ". Current version: " + m_version + ".");
+            }
+        }
+
+        @Override
+        public OnHeapStringWriteData createWrite(final int capacity) {
+            return new OnHeapStringWriteData(capacity);
+        }
+
+        @Override
+        public Field getField(final String name, final LongSupplier dictionaryIdSupplier) {
+            return Field.nullable(name, MinorType.VARCHAR.getType());
+        }
+
+        @Override
+        public void copyToVector(final NullableReadData data, final FieldVector fieldVector) {
+            var d = (OnHeapStringReadData)data; // TODO generic?
+            var vector = (VarCharVector)fieldVector;
+
+            // Encode strings if not all of them are encoded yet
+            var lastBytesIdx = d.m_offsets.lastWrittenIndex();
+            if (lastBytesIdx < d.length() - 1) {
+                encode(d.m_data, lastBytesIdx + 1, d.m_dataBytes, d.m_offsets, d.length() - lastBytesIdx - 1);
+            }
+
+            vector.allocateNew(d.m_dataBytes.length, d.length());
+
+            vector.getDataBuffer().setBytes(0, d.m_dataBytes);
+            d.m_validity.copyTo(vector.getValidityBuffer());
+            d.m_offsets.copyTo(vector.getOffsetBuffer());
+
+            vector.setLastSet(d.length() - 1);
+            vector.setValueCount(d.length());
+        }
+
+        @Override
+        public DictionaryProvider getDictionaries(final NullableReadData data) {
+            return null;
+        }
+
+        @Override
+        public int initialNumBytesPerElement() {
+            // TODO
+            return 1;
+        }
+    }
+}
