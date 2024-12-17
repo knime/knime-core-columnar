613cfe05c58e7973c0b8ec208d756ef3e898dae2
Benjamin Wilhelm <benjamin.wilhelm@knime.com>
2024-12-17 15:27:51 +0100

    Make it compile

--- org.knime.core.columnar.arrow.tests/src/test/java/org/knime/core/columnar/arrow/data/OnHeapComplexStructDataTest.java
+++ org.knime.core.columnar.arrow.tests/src/test/java/org/knime/core/columnar/arrow/data/OnHeapComplexStructDataTest.java
@@ -46,7 +46,7 @@
  * History
  *   Oct 14, 2020 (benjamin): created
  */
-package org.knime.core.columnar.arrow.data.old;
+package org.knime.core.columnar.arrow.data;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
@@ -54,25 +54,22 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
-import org.apache.arrow.vector.LargeVarBinaryVector;
-import org.apache.arrow.vector.complex.StructVector;
 import org.knime.core.columnar.arrow.AbstractArrowDataTest;
-import org.knime.core.columnar.arrow.data.old.ArrowStructData;
-import org.knime.core.columnar.arrow.data.old.ArrowDoubleData.ArrowDoubleDataFactory;
-import org.knime.core.columnar.arrow.data.old.ArrowDoubleData.ArrowDoubleReadData;
-import org.knime.core.columnar.arrow.data.old.ArrowDoubleData.ArrowDoubleWriteData;
-import org.knime.core.columnar.arrow.data.old.ArrowIntData.ArrowIntDataFactory;
-import org.knime.core.columnar.arrow.data.old.ArrowIntData.ArrowIntReadData;
-import org.knime.core.columnar.arrow.data.old.ArrowIntData.ArrowIntWriteData;
-import org.knime.core.columnar.arrow.data.old.ArrowStringData.ArrowStringDataFactory;
-import org.knime.core.columnar.arrow.data.old.ArrowStringData.ArrowStringReadData;
-import org.knime.core.columnar.arrow.data.old.ArrowStringData.ArrowStringWriteData;
-import org.knime.core.columnar.arrow.data.old.ArrowStructData.ArrowStructDataFactory;
-import org.knime.core.columnar.arrow.data.old.ArrowStructData.ArrowStructReadData;
-import org.knime.core.columnar.arrow.data.old.ArrowStructData.ArrowStructWriteData;
-import org.knime.core.columnar.arrow.data.old.ArrowVarBinaryData.ArrowVarBinaryDataFactory;
-import org.knime.core.columnar.arrow.data.old.ArrowVarBinaryData.ArrowVarBinaryReadData;
-import org.knime.core.columnar.arrow.data.old.ArrowVarBinaryData.ArrowVarBinaryWriteData;
+import org.knime.core.columnar.arrow.data.OnHeapDoubleData.OnHeapDoubleDataFactory;
+import org.knime.core.columnar.arrow.data.OnHeapDoubleData.OnHeapDoubleReadData;
+import org.knime.core.columnar.arrow.data.OnHeapDoubleData.OnHeapDoubleWriteData;
+import org.knime.core.columnar.arrow.data.OnHeapIntData.OnHeapIntDataFactory;
+import org.knime.core.columnar.arrow.data.OnHeapIntData.OnHeapIntReadData;
+import org.knime.core.columnar.arrow.data.OnHeapIntData.OnHeapIntWriteData;
+import org.knime.core.columnar.arrow.data.OnHeapStringData5000.OnHeapStringDataFactory;
+import org.knime.core.columnar.arrow.data.OnHeapStringData5000.OnHeapStringReadData;
+import org.knime.core.columnar.arrow.data.OnHeapStringData5000.OnHeapStringWriteData;
+import org.knime.core.columnar.arrow.data.OnHeapStructData.OnHeapStructDataFactory;
+import org.knime.core.columnar.arrow.data.OnHeapStructData.OnHeapStructReadData;
+import org.knime.core.columnar.arrow.data.OnHeapStructData.OnHeapStructWriteData;
+import org.knime.core.columnar.arrow.data.OnHeapVarBinaryData.OnHeapVarBinaryDataFactory;
+import org.knime.core.columnar.arrow.data.OnHeapVarBinaryData.OnHeapVarBinaryReadData;
+import org.knime.core.columnar.arrow.data.OnHeapVarBinaryData.OnHeapVarBinaryWriteData;
 import org.knime.core.columnar.data.DoubleData.DoubleReadData;
 import org.knime.core.columnar.data.DoubleData.DoubleWriteData;
 import org.knime.core.columnar.data.IntData.IntReadData;
@@ -85,51 +82,50 @@ import org.knime.core.columnar.data.VarBinaryData.VarBinaryReadData;
 import org.knime.core.columnar.data.VarBinaryData.VarBinaryWriteData;
 
 /**
- * Test {@link ArrowStructData} with a struct consisting of a string, integer, and struct (of varbinary and double).
+ * Test {@link OnHeapStructData} with a struct consisting of a string, integer, and struct (of varbinary and double).
  *
  * @author Benjamin Wilhelm, KNIME GmbH, Konstanz, Germany
  */
-public class ArrowComplexStructDataTest extends AbstractArrowDataTest<ArrowStructWriteData, ArrowStructReadData> {
+public class OnHeapComplexStructDataTest extends AbstractArrowDataTest<OnHeapStructWriteData, OnHeapStructReadData> {
 
     private static final int MAX_LENGTH = 100;
 
-    private static ArrowStructDataFactory createFactory() {
-        final ArrowStringDataFactory child1 = ArrowStringDataFactory.INSTANCE;
-        final ArrowIntDataFactory child2 = ArrowIntDataFactory.INSTANCE;
-        final ArrowStructDataFactory child3 =
-            new ArrowStructDataFactory(ArrowVarBinaryDataFactory.INSTANCE, ArrowDoubleDataFactory.INSTANCE);
-        return new ArrowStructDataFactory(child1, child2, child3);
+    private static OnHeapStructDataFactory createFactory() {
+        var child1 = OnHeapStringDataFactory.INSTANCE;
+        var child2 = OnHeapIntDataFactory.INSTANCE;
+        var child3 = new OnHeapStructDataFactory(OnHeapVarBinaryDataFactory.INSTANCE, OnHeapDoubleDataFactory.INSTANCE);
+        return new OnHeapStructDataFactory(child1, child2, child3);
     }
 
-    /** Create the test for {@link ArrowStructData} */
-    public ArrowComplexStructDataTest() {
+    /** Create the test for {@link OnHeapStructData} */
+    public OnHeapComplexStructDataTest() {
         super(createFactory());
     }
 
     @Override
-    protected ArrowStructWriteData castW(final Object o) {
-        assertTrue(o instanceof ArrowStructWriteData);
-        return (ArrowStructWriteData)o;
+    protected OnHeapStructWriteData castW(final Object o) {
+        assertTrue(o instanceof OnHeapStructWriteData);
+        return (OnHeapStructWriteData)o;
     }
 
     @Override
-    protected ArrowStructReadData castR(final Object o) {
-        assertTrue(o instanceof ArrowStructReadData);
-        return (ArrowStructReadData)o;
+    protected OnHeapStructReadData castR(final Object o) {
+        assertTrue(o instanceof OnHeapStructReadData);
+        return (OnHeapStructReadData)o;
     }
 
     @Override
-    protected void setValue(final ArrowStructWriteData data, final int index, final int seed) {
+    protected void setValue(final OnHeapStructWriteData data, final int index, final int seed) {
         final StringWriteData data0 = data.getWriteDataAt(0);
-        assertTrue(data0 instanceof ArrowStringWriteData);
+        assertTrue(data0 instanceof OnHeapStringWriteData);
         final IntWriteData data1 = data.getWriteDataAt(1);
-        assertTrue(data1 instanceof ArrowIntWriteData);
+        assertTrue(data1 instanceof OnHeapIntWriteData);
         final StructWriteData data2 = data.getWriteDataAt(2);
-        assertTrue(data2 instanceof ArrowStructWriteData);
+        assertTrue(data2 instanceof OnHeapStructWriteData);
         final VarBinaryWriteData data20 = data2.getWriteDataAt(0);
-        assertTrue(data20 instanceof ArrowVarBinaryWriteData);
+        assertTrue(data20 instanceof OnHeapVarBinaryWriteData);
         final DoubleWriteData data21 = data2.getWriteDataAt(1);
-        assertTrue(data21 instanceof ArrowDoubleWriteData);
+        assertTrue(data21 instanceof OnHeapDoubleWriteData);
 
         data0.setString(index, Integer.toString(seed));
         data1.setInt(index, seed);
@@ -138,17 +134,17 @@ public class ArrowComplexStructDataTest extends AbstractArrowDataTest<ArrowStruc
     }
 
     @Override
-    protected void checkValue(final ArrowStructReadData data, final int index, final int seed) {
+    protected void checkValue(final OnHeapStructReadData data, final int index, final int seed) {
         final StringReadData data0 = data.getReadDataAt(0);
-        assertTrue(data0 instanceof ArrowStringReadData);
+        assertTrue(data0 instanceof OnHeapStringReadData);
         final IntReadData data1 = data.getReadDataAt(1);
-        assertTrue(data1 instanceof ArrowIntReadData);
+        assertTrue(data1 instanceof OnHeapIntReadData);
         final StructReadData data2 = data.getReadDataAt(2);
-        assertTrue(data2 instanceof ArrowStructReadData);
+        assertTrue(data2 instanceof OnHeapStructReadData);
         final VarBinaryReadData data20 = data2.getReadDataAt(0);
-        assertTrue(data20 instanceof ArrowVarBinaryReadData);
+        assertTrue(data20 instanceof OnHeapVarBinaryReadData);
         final DoubleReadData data21 = data2.getReadDataAt(1);
-        assertTrue(data21 instanceof ArrowDoubleReadData);
+        assertTrue(data21 instanceof OnHeapDoubleReadData);
 
         assertEquals(Integer.toString(seed), data0.getString(index));
         assertEquals(seed, data1.getInt(index));
@@ -157,25 +153,13 @@ public class ArrowComplexStructDataTest extends AbstractArrowDataTest<ArrowStruc
     }
 
     @Override
-    protected boolean isReleasedW(final ArrowStructWriteData data) {
-        return data.m_vector == null;
+    protected boolean isReleasedW(final OnHeapStructWriteData data) {
+        return false;
     }
 
     @Override
-    @SuppressWarnings("resource")
-    protected boolean isReleasedR(final ArrowStructReadData data) {
-        final ArrowStructReadData d = castR(data);
-        final ArrowStructReadData d2 = d.getReadDataAt(2);
-        final ArrowVarBinaryReadData d20 = d2.getReadDataAt(0);
-
-        // We just check the validity buffer of this data, of the inner struct
-        // and of the 2nd level object data
-        final StructVector vector = d.m_vector;
-        final StructVector vector2 = d2.m_vector;
-        final LargeVarBinaryVector vector20 = d20.m_vector;
-
-        return vector.getValidityBuffer().capacity() == 0 && vector2.getValidityBuffer().capacity() == 0
-            && vector20.getValidityBuffer().capacity() == 0;
+    protected boolean isReleasedR(final OnHeapStructReadData data) {
+        return false;
     }
 
     @Override
