9006febb6b9c16201aedd627e71edf7d69120a7c
tpietzsch <tobias.pietzsch@gmail.com>
2025-07-15 23:57:45 +0200

    AP-24030: move debug(...) methods to separate class DebugLog
    Also add a DebugLog.debug() variant with long argument. This avoids
    unnecessary Long boxing if debug messages are disabled.

---
+++ org.knime.core.columnar/src/main/java/org/knime/core/columnar/badger/DebugLog.java
@@ -0,0 +1,106 @@
+/*
+ * ------------------------------------------------------------------------
+ *
+ *  Copyright by KNIME AG, Zurich, Switzerland
+ *  Website: http://www.knime.com; Email: contact@knime.com
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License, Version 3, as
+ *  published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful, but
+ *  WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ *  Additional permission under GNU GPL version 3 section 7:
+ *
+ *  KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ *  Hence, KNIME and ECLIPSE are both independent programs and are not
+ *  derived from each other. Should, however, the interpretation of the
+ *  GNU GPL Version 3 ("License") under any applicable laws result in
+ *  KNIME and ECLIPSE being a combined program, KNIME AG herewith grants
+ *  you the additional permission to use and propagate KNIME together with
+ *  ECLIPSE with only the license terms in place for ECLIPSE applying to
+ *  ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ *  license terms of ECLIPSE themselves allow for the respective use and
+ *  propagation of ECLIPSE together with KNIME.
+ *
+ *  Additional permission relating to nodes for KNIME that extend the Node
+ *  Extension (and in particular that are based on subclasses of NodeModel,
+ *  NodeDialog, and NodeView) and that only interoperate with KNIME through
+ *  standard APIs ("Nodes"):
+ *  Nodes are deemed to be separate and independent programs and to not be
+ *  covered works.  Notwithstanding anything to the contrary in the
+ *  License, the License does not apply to Nodes, you are not required to
+ *  license Nodes under the License, and you are granted a license to
+ *  prepare and propagate Nodes, in each case even if such Nodes are
+ *  propagated with or for interoperation with KNIME.  The owner of a Node
+ *  may freely choose the license terms applicable to such Node, including
+ *  when such Node is propagated with or for interoperation with KNIME.
+ * ---------------------------------------------------------------------
+ *
+ * History
+ *   15 Jul 2025 (pietzsch): created
+ */
+package org.knime.core.columnar.badger;
+
+import org.slf4j.helpers.MessageFormatter;
+
+/**
+ * (Optional) logging for HeapBadger debugging.
+ * 
+ * @author Tobias Pietzsch
+ */
+class DebugLog {
+    private static final boolean DEBUG = false;
+
+    static void debug(final String msg) {
+        if (DEBUG) {
+            System.out.println(msg);
+        }
+    }
+
+    static void debug(final String format, final Object arg) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg).getMessage());
+        }
+    }
+
+    static void debug(final String format, final long arg) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg).getMessage());
+        }
+    }
+
+    static void debug(final String format, final int arg) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg).getMessage());
+        }
+    }
+
+    static void debug(final String format, final boolean arg) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg).getMessage());
+        }
+    }
+
+    static void debug(final String format, final int arg1, final int arg2) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg1, arg2).getMessage());
+        }
+    }
+
+    static void debug(final String format, final Object arg1, final int arg2) {
+        if (DEBUG) {
+            System.out.println(MessageFormatter.format(format, arg1, arg2).getMessage());
+        }
+    }
+
+    private DebugLog() {
+        // Do not instantiate
+    }
+}
