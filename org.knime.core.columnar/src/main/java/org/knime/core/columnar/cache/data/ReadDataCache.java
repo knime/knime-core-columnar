d73fcdb2c9564dcfebb7e3e7fc92279bb05f9322
tpietzsch <tobias.pietzsch@gmail.com>
2025-08-15 17:49:20 +0200

    AP-24030: Revise ReadDataCache Future chaining and close() logic

--- org.knime.core.columnar/src/main/java/org/knime/core/columnar/cache/data/ReadDataCache.java
+++ org.knime.core.columnar/src/main/java/org/knime/core/columnar/cache/data/ReadDataCache.java
@@ -111,9 +111,8 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
             final var batchId = m_numBatches;
             m_currentlyWritingBatches.putRetained(batchId, batch);
 
-            handleDoneFuture();
-
-            enqueueRunnable(() -> { // NOSONAR
+            m_futureQueue.handleDoneFuture();
+            m_futureQueue.enqueueRunnable(() -> { // NOSONAR
                 try {
                     if (!m_closed.get()) {
                         // No need to write to delegate if we're closed already.
@@ -135,30 +134,47 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
             m_numBatches++;
         }
 
-        @Override
-        public synchronized void close() {
-            handleDoneFuture();
-            enqueueRunnable(() -> {
-                try {
-                    m_writerDelegate.close();
-                } catch (IOException e) {
-                    throw new IllegalStateException("Failed to close writer.", e);
-                }
-            });
-        }
+        private final AtomicBoolean m_closed = new AtomicBoolean(false);
 
-        private void handleDoneFuture() {
-            if (m_future.isDone()) {
-                try {
-                    waitForAndHandleFuture();
-                } catch (InterruptedException e) {
-                    // Restore interrupted state
-                    Thread.currentThread().interrupt();
-                    // since we just checked whether the future is done, we likely never end up in this code block
-                    throw new IllegalStateException(ERROR_ON_INTERRUPT, e);
-                }
+        @Override
+        public synchronized void close() throws IOException {
+            if (!m_closed.getAndSet(true)) {
+                m_futureQueue.handleDoneFuture();
+                m_futureQueue.enqueueRunnable(() -> {
+                    try {
+                        m_writerDelegate.close();
+                    } catch (IOException e) {
+                        throw new IllegalStateException("Failed to close writer.", e);
+                    }
+                });
+                waitForAllTasksToFinish();
             }
         }
+
+        private void waitForAllTasksToFinish() throws IOException {
+            // We use a {@link CountDownLatch} to _really_ wait for all tasks to finish because
+            // flush() could be interrupted or pick up that the interrupt flag was set. Unfortunately,
+            // the {@link CompletableFuture}s will still continue to be executed, hence we wait with
+            // a latch that is counted down after everything else.
+            final var closedLatch = new CountDownLatch(1);
+            m_futureQueue.enqueueRunnable(closedLatch::countDown);
+            m_futureQueue.enqueueExceptionHandler(ex -> closedLatch.countDown()); // also count down in case of a previous exception
+            try {
+                closedLatch.await();
+            } catch (InterruptedException e) {
+                LOGGER.debug("Interrupted while waiting for tasks to finish");
+            }
+
+            try {
+                // There should not be any tasks left after the previous block,
+                // so this should return immediately. But we still invoke m_future.get()
+                // to notice whether exceptions were thrown.
+                m_futureQueue.waitForAndHandleFuture();
+            } catch (InterruptedException e) {
+                throw new IOException("Close should not be interrupted!", e);
+            }
+        }
+
 
         @Override
         public int initialNumBytesPerElement() {
@@ -202,7 +218,9 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
                 missingCols = Arrays.copyOf(missingCols, numMissing);
 
                 try {
-                    waitForAndHandleFuture();
+                    // wait until everything written so far is flushed
+                    // TODO (TP)): explain why?
+                    m_futureQueue.waitForAndHandleFuture();
                 } catch (InterruptedException e) {
                     // At this point we already m_globalCache.getRetained() all datas[i] != null.
                     // These need to be released before re-throwing the Exception.
@@ -254,18 +272,15 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
         public void close() throws IOException {
             // no resources held
         }
-
     }
 
     private final ReadDataCacheWriter m_writer;
 
     private final RandomAccessBatchReadable m_readableDelegate;
 
-    private final ExecutorService m_executor;
-
     private final EvictingCache<ColumnDataUniqueId, NullableReadData> m_globalCache;
 
-    private CompletableFuture<Void> m_future = CompletableFuture.completedFuture(null);
+    private final FutureQueue m_futureQueue;
 
     private final Map<ColumnDataUniqueId, Object> m_cachedDataIds = new ConcurrentHashMap<>();
 
@@ -273,55 +288,19 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
 
     private final ReadBatchRetainingMap m_currentlyWritingBatches = new ReadBatchRetainingMap();
 
-    private final AtomicBoolean m_closed = new AtomicBoolean(false);
-
     /**
      * @param writable the delegate to which to write asynchronously
      * @param readable the delegate from which to read in case of a cache miss
      * @param cache the cache for storing data
      * @param executor the executor to which to submit asynchronous writes to the delegate
      */
-    @SuppressWarnings("resource")
     public ReadDataCache(final BatchWritable writable, final RandomAccessBatchReadable readable,
         final SharedReadDataCache cache, final ExecutorService executor) {
 
         m_writer = new ReadDataCacheWriter(writable.getWriter());
         m_readableDelegate = readable;
         m_globalCache = cache.getCache();
-        m_executor = executor;
-    }
-
-    synchronized void enqueueRunnable(final Runnable r) {
-        m_future = m_future.thenRunAsync(r, m_executor);
-    }
-
-    /**
-     * Add a runnable to the chain of futures that is executed if and only if any of the previous futures terminated
-     * exceptionally.
-     */
-    synchronized void enqueueExceptionHandler(final Consumer<Throwable> handler) {
-        m_future = m_future.exceptionallyAsync((final Throwable exception) -> {
-            handler.accept(exception);
-            return null; // Need to return null because a (void) return value is expected here
-        }, m_executor);
-    }
-
-    void waitForAndHandleFuture() throws InterruptedException {
-        try {
-            m_future.get();
-        } catch (final ExecutionException e) {
-            throw wrap(e.getCause());
-        }
-    }
-
-    private static RuntimeException wrap(final Throwable t) {
-        final String error;
-        if (t.getMessage() != null) {
-            error = t.getMessage();
-        } else {
-            error = "Failed to asynchronously write cached rows to file.";
-        }
-        return new IllegalStateException(error, t);
+        m_futureQueue = new FutureQueue(executor);
     }
 
     @Override
@@ -332,7 +311,7 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
     @Override
     public void flush() throws IOException {
         try {
-            waitForAndHandleFuture();
+            m_futureQueue.waitForAndHandleFuture();
         } catch (InterruptedException e) {
             // Restore interrupted state...
             Thread.currentThread().interrupt();
@@ -352,50 +331,11 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
 
     @Override
     public synchronized void close() throws IOException {
-        if (!m_closed.getAndSet(true)) {
-
-            waitForAllTasksToFinish();
-
-            m_writer.close();
-            try {
-                // Wait for close to finish
-                waitForAndHandleFuture();
-            } catch (InterruptedException e) {
-                // The interrupt status should have been reset by some previous wait operation,
-                // so if we get interrupted here, something has gone wrong and we throw an exception.
-                throw new IOException("Close should not be interrupted!", e);
-            }
-
-            releaseAllReferencedData();
-
-            m_currentlyWritingBatches.clear();
-            m_cachedDataIds.clear();
-            m_readableDelegate.close();
-        }
-    }
-
-    private void waitForAllTasksToFinish() throws IOException {
-        // We use a {@link CountDownLatch} to _really_ wait for all tasks to finish because
-        // flush() could be interrupted or pick up that the interrupt flag was set. Unfortunately,
-        // the {@link CompletableFuture}s will still continue to be executed, hence we wait with
-        // a latch that is counted down after everything else.
-        final var closedLatch = new CountDownLatch(1);
-        enqueueRunnable(closedLatch::countDown);
-        enqueueExceptionHandler(ex -> closedLatch.countDown()); // also count down in case of a previous exception
-        try {
-            closedLatch.await();
-        } catch (InterruptedException e) {
-            LOGGER.debug("Interrupted while waiting for tasks to finish");
-        }
-
-        try {
-            // There should not be any tasks left after the previous block,
-            // so this should return immediately. But we still invoke m_future.get()
-            // to notice whether exceptions were thrown.
-            waitForAndHandleFuture();
-        } catch (InterruptedException e) {
-            throw new IOException("Close should not be interrupted!", e);
-        }
+        m_writer.close();
+        releaseAllReferencedData();
+        m_currentlyWritingBatches.clear();
+        m_cachedDataIds.clear();
+        m_readableDelegate.close();
     }
 
     private void releaseAllReferencedData() {
@@ -446,6 +386,71 @@ public final class ReadDataCache implements BatchWritable, RandomAccessBatchRead
             }
             batch.retain();
             return batch;
+        }
+    }
+
+    /**
+     * Helper class for managing a chain of Futures
+     */
+    private static final class FutureQueue
+    {
+        private volatile CompletableFuture<Void> m_future = CompletableFuture.completedFuture(null);
+
+        private final ExecutorService m_executor;
+
+        FutureQueue(final ExecutorService executor) {
+            m_executor = executor;
+        }
+
+        synchronized void enqueueRunnable(final Runnable r) {
+            m_future = m_future.thenRunAsync(r, m_executor);
+        }
+
+        /**
+         * Add a runnable to the chain of futures that is executed if and only if any of the previous futures terminated
+         * exceptionally.
+         */
+        synchronized void enqueueExceptionHandler(final Consumer<? super Throwable> handler) {
+            m_future = m_future.exceptionallyAsync(throwable -> {
+                handler.accept(throwable);
+                return null; // Need to return null because a (void) return value is expected here
+            }, m_executor);
+        }
+
+        void waitForAndHandleFuture() throws InterruptedException {
+            try {
+                m_future.get();
+            } catch (final ExecutionException e) {
+                throw wrap(e.getCause());
+            }
+        }
+
+        void handleDoneFuture() {
+            final CompletableFuture<Void> future = m_future;
+            if (future.isDone()) {
+                try {
+                    try {
+                        future.get();
+                    } catch (final ExecutionException e) {
+                        throw wrap(e.getCause());
+                    }
+                } catch (InterruptedException e) {
+                    // Restore interrupted state
+                    Thread.currentThread().interrupt();
+                    // since we just checked whether the future is done, we likely never end up in this code block
+                    throw new IllegalStateException(ERROR_ON_INTERRUPT, e);
+                }
+            }
+        }
+
+        private static RuntimeException wrap(final Throwable t) {
+            final String error;
+            if (t.getMessage() != null) {
+                error = t.getMessage();
+            } else {
+                error = "Failed to asynchronously write cached rows to file.";
+            }
+            return new IllegalStateException(error, t);
         }
     }
 
