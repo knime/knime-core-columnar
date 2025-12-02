6cc3050fa60e7c49024252a30169b37e46c954d0
tpietzsch <tobias.pietzsch@gmail.com>
2025-12-02 10:50:49 +0100

    AP-24030: Revise ReadDataCache
    (*) Extract chaining of CompletableFuture into static inner class ReadDataCache.FutureQueue.
        The synchronized methods enqueueRunnable(...) and enqueueExceptionHandler(...)
        now synchronize on the FutureQueue instance instead of the ReadDataCache. They
        no longer interfere with the synchronized ReadDataCache.close() method or
        external synchronization on the ReadDataCache instance. This avoids potential
        deadlocks I ran into (but I don't remember the exact circumstances).
    
    (*) Fix minor bug causing duplicate work in ReadData(Read)CacheReader.readRetained():
        If two threads were trying to read the same batch from the delegate reader at
        the same time, they would synchronize to read that batch sequentially. However,
        they would still both read the batch, instead of the second thread using the
        data that the first thread put into the cache.
    
    (*) General revision and cleaning up of close() logic and synchronization.
    
    (*) Let ReadDataCache extend ReadDataReadCache:
        ReadDataReadCache is responsible for managing the cache and reading from the
        delegate. ReadDataCache just adds writing logic on top of that.
    
    (*) SharedReadDataCache uses plain Guava Cache instead of EvictingCache.
        (We don't need evictors here at all, so this is hopefully simpler to understand
        and less overhead.)

--- org.knime.core.columnar/src/main/java/org/knime/core/columnar/cache/data/SharedReadDataCache.java
+++ org.knime.core.columnar/src/main/java/org/knime/core/columnar/cache/data/SharedReadDataCache.java
@@ -48,48 +48,89 @@
  */
 package org.knime.core.columnar.cache.data;
 
-import org.knime.core.columnar.cache.ColumnDataUniqueId;
-import org.knime.core.columnar.cache.EvictingCache;
-import org.knime.core.columnar.cache.SizeBoundLruCache;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.knime.core.columnar.ReferencedData;
 import org.knime.core.columnar.data.NullableReadData;
 import org.knime.core.columnar.memory.ColumnarOffHeapMemoryAlertSystem;
 import org.knime.core.columnar.store.UseOnHeapColumnStoreProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.Weigher;
+
 /**
  * A cache for storing data that can be shared between multiple {@link ReadDataCache ReadDataCaches} and
  * {@link ReadDataReadCache ReadDataReadCaches}.
  *
  * @author Marc Bux, KNIME GmbH, Berlin, Germany
+ * @author Tobias Pietzsch
  */
 public final class SharedReadDataCache {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SharedReadDataCache.class);
 
-    private final EvictingCache<ColumnDataUniqueId, NullableReadData> m_cache;
+    /**
+     * A key in the shared cache: The {@code owner} of the associated {@code NullableReadData} value, its column index,
+     * and batch index
+     *
+     * @param owner who put the value into the cache
+     * @param columnIndex column index
+     * @param batchIndex batch index
+     */
+    record ColumnDataKey(DataOwner owner, int columnIndex, int batchIndex) {}
+
+    private final Cache<ColumnDataKey, NullableReadData> m_cache;
 
     private final long m_cacheSizeBytes;
 
     /**
-     * @param cacheSizeBytes the size of the cache in bytes
+     * @param cacheSizeBytes the maximum size of the cache in bytes
      * @param concurrencyLevel the allowed concurrency among update operations
      */
     public SharedReadDataCache(final long cacheSizeBytes, final int concurrencyLevel) {
-        m_cache = new SizeBoundLruCache<>(cacheSizeBytes, concurrencyLevel);
+
+        final Weigher<ColumnDataKey, NullableReadData> weigher = (k, v) -> {
+            final long size = v.sizeOf();
+            if (size > Integer.MAX_VALUE) {
+                final Logger logger = LoggerFactory.getLogger(getClass());
+                logger.error("Size of data ({}) is larger than maximum ({}).", size, Integer.MAX_VALUE);
+                return Integer.MAX_VALUE;
+            }
+            return Math.max(1, (int)size);
+        };
+
+        final RemovalListener<ColumnDataKey, NullableReadData> removalListener = removalNotification -> {
+            final NullableReadData data = removalNotification.getValue();
+            if (data != null) {
+                data.release();
+            }
+            removalNotification.getKey().owner().decrement();
+        };
+
+        m_cache = CacheBuilder.newBuilder() //
+                .concurrencyLevel(concurrencyLevel) //
+                .maximumWeight(cacheSizeBytes) //
+                .weigher(weigher) //
+                .removalListener(removalListener) //
+                .build();
+
         m_cacheSizeBytes = cacheSizeBytes;
 
         if (!UseOnHeapColumnStoreProperty.useOnHeapColumnStore()) {
-            ColumnarOffHeapMemoryAlertSystem.INSTANCE.addMemoryListener(() -> {
-                return clear();
-            });
+            ColumnarOffHeapMemoryAlertSystem.INSTANCE.addMemoryListener(this::clear);
         }
     }
 
     /**
+     * Get the maximum size of this cache in bytes.
+     *
      * @return maximum size of cache in bytes.
      */
-    public final long getMaxSizeInBytes() {
+    public long getMaxSizeInBytes() {
         return m_cacheSizeBytes;
     }
 
@@ -103,16 +144,143 @@ public final class SharedReadDataCache {
         if (numEntries > 0) {
             LOGGER.info("Received memory alert. Clearing approximately {} entries.", numEntries);
             m_cache.invalidateAll();
+            m_cache.cleanUp();
             return true;
         }
         return false;
     }
 
+    /**
+     * Returns the approximate number of key-data mappings in this cache.
+     *
+     * @return the approximate number of key-data mappings in this cache
+     */
     int size() {
-        return m_cache.size();
+        return m_cache.asMap().size();
     }
 
-    EvictingCache<ColumnDataUniqueId, NullableReadData> getCache() {
-        return m_cache;
+    /**
+     * Associates the given data with the given key and places it in the cache.
+     * <p>
+     * The given {@code value} is {@link ReferencedData#retain() retained} for the cache, and will be
+     * {@link ReferencedData#release() released} when it is evicted from the cache.
+     * <p>
+     * If another value was already associated with the same key, the cache will {@link ReferencedData#release()
+     * release} it.
+     *
+     * @param key key with which the specified data is to be associated
+     * @param value data to be retained and associated with the specified key
+     */
+    void put(final ColumnDataKey key, final NullableReadData value) {
+        value.retain();
+        key.owner().increment();
+        m_cache.put(key, value);
+    }
+
+    /**
+     * Retain and return the value associated with the specified {@code key}. Returns {@code null} if this cache
+     * contains no mapping for the key.
+     * <p>
+     * The returned data is {@link ReferencedData#retain() retained} for the caller. It is up to the caller to
+     * {@link ReferencedData#release() release} the returned data once it is no longer needed.
+     *
+     * @param key the key
+     * @return retained data to which the specified key is mapped, or null if this cache contains no mapping for the key
+     */
+    NullableReadData getRetained(final ColumnDataKey key) {
+        final NullableReadData cached = m_cache.getIfPresent(key);
+        if (cached != null) {
+            try {
+                cached.retain();
+                return cached;
+            } catch (IllegalStateException e) { // NOSONAR
+                // we should only end up here in the very rare case where the data is evicted in between get() and retain()
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Removes all entries belonging to {@code owner}. Removed values are {@link ReferencedData#release() released}.
+     * <p>
+     * This method blocks until all references ever held by this cache to values belonging to {@code owner} have been released.
+     */
+    void invalidateAll(final DataOwner owner) {
+        m_cache.asMap().keySet().removeIf(key -> key.owner() == owner);
+        m_cache.cleanUp();
+        owner.await();
+    }
+
+    /**
+     * A handle that identifies (as part of {@link ColumnDataKey}) the owner of an entry in the
+     * {@code SharedReadDataCache}.
+     * <p>
+     * Each of the {@link ReadDataReadCache ReadDataReadCaches} creates one of these, and uses it to identify itself as
+     * the owner of data that it puts into the shared cache. (For this purpose we use object identity, so every
+     * {@code DataOwner} instance represents a distinct owner.)
+     */
+    static final class DataOwner {
+
+        // NB: For cleanup, the DataOwner is used as a synchronization mechanism
+        // similar to CountUpDownLatch, but more lightweight.
+        //
+        // When a ReadDataReadCache is closed, all data that it owns must be
+        // removed from the global cache, and references held by the global
+        // cache released. Releasing happens in the RemovalListener (and must
+        // happen there, because it also needs to trigger if entries are evicted
+        // due to size constraints, etc).
+        //
+        // Before the ReadDataReadCache can close its delegate, we must be sure
+        // that all references have been released.
+        //
+        // There is no way to guarantee this without extra synchronization:
+        // RemovalNotifications are not necessarily handled by the thread that
+        // removes the entries. In invalidateAll(DataOwner) we call
+        // m_cache.cleanUp() which makes sure that all pending RemovalListeners
+        // are handled. However, before we get to call cleanUp(), another thread
+        // might have snatched (some) pending RemovalNotifications and, if we
+        // are unlucky, take longer to handle them than our cleanUp(). In that
+        // rare case we would return from invalidateAll(DataOwner) before all
+        // references were released, and if we are quick enough to close the
+        // underlying Arrow allocator, we run into memory leaks.
+        //
+        // To avoid that, every time an entry is put into the cache, the count
+        // of its owner is incremented. Every time an entry is released (evicted
+        // or explicitly removed), the count of its owner is decremented. At the
+        // end of invalidateAll(DataOwner), we await the count to reach 0.
+
+        private final AtomicInteger count = new AtomicInteger(1);
+
+        private final Object monitor = new Object();
+
+        private void increment() {
+            count.incrementAndGet();
+        }
+
+        private void decrement() {
+            if (count.decrementAndGet() == 0) {
+                synchronized (monitor) {
+                    monitor.notifyAll();
+                }
+            }
+        }
+
+        private void await() {
+            decrement();
+            synchronized (monitor) {
+                while (count.get() != 0) {
+                    try {
+                        monitor.wait();
+                    } catch (InterruptedException ignored) {
+                    }
+                }
+            }
+
+            // NB: We reset the count to 1, so that the DataOwner can be re-used
+            // (increments and decrements followed by await again) in principle.
+            // In practice, we don't reuse DataOwner currently, but better to be
+            // sure...
+            count.set(1);
+        }
     }
 }
